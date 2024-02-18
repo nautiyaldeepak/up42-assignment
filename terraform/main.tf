26a11b20a44240ab1b6f60832e725905c014e8ab
Deepak Nautiyal <deepaknautiyal@Deepaks-MacBook-Pro.local>
2024-02-18 21:55:18 +0530

    updating main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,28 @@
 resource "helm_release" "minio" {
   name       = "minio"
   chart      = "../helm-charts/minio"
+
+  values = [
+    "${file("../helm-charts/minio/values-prod.yaml")}"
+  ]
 }
 
 resource "helm_release" "s3www" {
   name       = "s3www"
   chart      = "../helm-charts/s3www"
-}
 
-resource "kubernetes_namespace" "monitoring" {
-  metadata {
-    name = "monitoring"
-  }
+  values = [
+    "${file("../helm-charts/s3www/values-prod.yaml")}"
+  ]
 }
 
 resource "helm_release" "prometheus-operator" {
   name       = "prometheus-operator"
   chart      = "../helm-charts/prometheus-operator"
   namespace  = "monitoring"
+  create_namespace  = "true"
+
+  values = [
+    "${file("../helm-charts/prometheus-operator/values-prod.yaml")}"
+  ]
 }
